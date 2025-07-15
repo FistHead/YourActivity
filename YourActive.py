997765211aa27dc208e65382baede9d348793045
FistHead <nokick2806@gmail.com>
2025-07-15 20:22:15 +0300

    Update YourActive.py

--- YourActive.py
+++ YourActive.py
@@ -61,16 +61,16 @@ class App(customtkinter.CTk):
         self.time_in_pc = ''
 
 
-        self.pc_work_time_text = customtkinter.CTkLabel(master= self, text=f"ВРЕМЯ РАБОТЫ ПК: {pc_work_time}", fg_color="transparent", justify = "center",width = 600,height=50,font=font)
+        self.pc_work_time_text = customtkinter.CTkLabel(master= self, text=f"PC WORKING TIME: {pc_work_time}", fg_color="transparent", justify = "center",width = 600,height=50,font=font)
         self.pc_work_time_text.place(relx=0.5, rely=0.2, anchor= "center")
 
-        self.pc_boot_time_text = customtkinter.CTkLabel(master= self, text=f"КОМПЬЮТЕР БЫЛ ЗАПУЩЕН В: {wakeup_time}", fg_color="transparent", justify = "center",width = 600,height=50,font=font)
+        self.pc_boot_time_text = customtkinter.CTkLabel(master= self, text=f"COMPUTER WAS STARTED IN: {wakeup_time}", fg_color="transparent", justify = "center",width = 600,height=50,font=font)
         self.pc_boot_time_text.place(relx=0.5, rely=0.3, anchor= "center")
 
         self.pc_current_time_text = customtkinter.CTkLabel(master= self, text= '', fg_color="transparent", justify = "center",width = 600,height=50,font=font)
         self.pc_current_time_text.place(relx=0.5, rely=0.4, anchor= "center")
 
-        self.checkbox= customtkinter.CTkCheckBox(self, text= 'включить отображение времени',font=font,fg_color = 'white',border_color = 'white',corner_radius = 0,hover = False,variable=self.checkbox_var,command=self.toggle_time_window)
+        self.checkbox= customtkinter.CTkCheckBox(self, text= 'turn on the time display',font=font,fg_color = 'white',border_color = 'white',corner_radius = 0,hover = False,variable=self.checkbox_var,command=self.toggle_time_window)
         self.checkbox.place(relx=0.5, rely=0.6, anchor= "center")
 
 
@@ -88,8 +88,8 @@ class App(customtkinter.CTk):
         date_time_obj2 = datetime.datetime.strptime(current_time, '%H:%M')
         self.time_in_pc = date_time_obj2 - date_time_obj1
 
-        self.pc_work_time_text.configure(text=f"ВРЕМЯ РАБОТЫ ПК: {formatted_time}")
-        self.pc_current_time_text.configure(text=f"ВРЕМЯ ПРОВЕДЕННОЕ ЗА ПК: {self.time_in_pc}")
+        self.pc_work_time_text.configure(text=f"PC WORKING TIME: {formatted_time}")
+        self.pc_current_time_text.configure(text=f"TIME SPENT ON THE PC: {self.time_in_pc}")
 
         if self.time_window and self.time_window.winfo_exists():
             self.time_window.pc_current_time_text.configure(text=f"{self.time_in_pc}")
